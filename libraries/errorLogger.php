9710018c01df948600f0b05455e40de0e1f106eb
Michael Schmidt <mschmidt74@hotmail.com>
2014-06-30 22:04:40 -0400

    Update errorLogger.php
        $this->error = $errorType->getMessage();
    
    Don't set the injected instance to just one method.  What if u later add more methods?  You want access to those!

--- libraries/errorLogger.php
+++ libraries/errorLogger.php
@@ -16,7 +16,7 @@ class ErrorLogger
 	 */
     public function __construct(ErrorInterface $errorType)
     {
-        $this->error = $errorType->getMessage();
+        $this->error = $errorType;
     }
 
 	/**
@@ -40,6 +40,6 @@ class ErrorLogger
     {
         $currentTimeStamp = date('Y-m-d H:i:s', time());
         return '[' . $currentTimeStamp . '] ' . $_SERVER['HTTP_USER_AGENT'] . ' - Client: ' .
-            $_SERVER['REMOTE_ADDR'] . ' - ' . $this->error . "\n";
+            $_SERVER['REMOTE_ADDR'] . ' - ' . $this->error->getMessage . "\n";
     }
 }
